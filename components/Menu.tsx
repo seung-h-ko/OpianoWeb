a7b2a7ba52d62d8ea1025b99acdb273be34f8b4b
Seung Hun Ko <seung.h.ko@outlook.com>
2023-05-07 19:07:44 -0400

    menu change

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -56,7 +56,8 @@ export default function Menu() {
                 transition={{
                     duration: 0.8,
                 }}
-                className='w-full flex overflow-x-scroll snap-mandatory overflow-y-hidden snap-x z-10 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#ffe7b3]/80 scroll-smooth'>
+                className='w-full flex overflow-x-scroll snap-mandatory overflow-y-hidden snap-x z-10 scrollbar-thin
+                 scrollbar-track-gray-400/20 scrollbar-thumb-[#ffe7b3]/80 scroll-smooth'>
 
                 <div
                     id='menu#0'
@@ -126,20 +127,20 @@ export default function Menu() {
                 </div>
 
             </motion.div>
-            <div className='h-[25%] bg-white'>
+            <div className='sm:h-36 h-36'>
             </div>
             <div className='absolute left-[5%] bottom-1/2 text-[#ffe7b3] z-20'>
                 <Link
                     onClick={handleLeftClick} href={number != 0 ? `#menu#${number - 1}` : '#menu#0'}
                 >
                     <ChevronDoubleLeftIcon
-                        className='h-10 w-10 active:scale-75 hidden md:inline-block' />
+                        className='h-10 w-10 active:scale-75 hidden sm:inline-block' />
                 </Link>
             </div>
             <div className='absolute right-[5%] bottom-1/2 text-[#ffe7b3] z-20'>
                 <Link onClick={handleRightClick} href={number != 6 - 1 ? `#menu#${number + 1}` : `#menu#${6 - 1}`}>
                     <ChevronDoubleRightIcon
-                        className='h-10 w-10 active:scale-75 hidden md:inline-block' />
+                        className='h-10 w-10 active:scale-75 hidden sm:inline-block' />
                 </Link>
             </div>
         </div>
